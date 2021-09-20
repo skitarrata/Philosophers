fa91765bebf288a6774cc6fe002e3f60f26762b0
skitarrata <skitarrata96@gmail.com>
2021-09-20 15:38:28 +0200

    init

---
+++ philo/header/philosophers.h
@@ -0,0 +1,14 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "error.h"
+
+typedef struct	s_philo
+{
+
+}				t_philo;
+
+#endif
